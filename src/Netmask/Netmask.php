d3fc44da39da07dc735ecc280c00b348b3b0cf49
Gabriel Tartaglia <ga.tartaglia96@gmail.com>
2016-01-19 12:56:34 -0200

    Replica lógica do módulo node-netmask e faz testes relacionados

--- src/Netmask/Netmask.php
+++ src/Netmask/Netmask.php
@@ -5,8 +5,110 @@ class Netmask
 {
     public $base;
     public $mask;
+    public $bitmask;
+    public $hostmask;
+    public $broadcast;
+    public $size;
+    public $first;
+    public $last;
 
-    public function __construct($net, $mask = null)
+    protected $netLong;
+    protected $maskLong;
+
+    public function __construct($net)
     {
+        list($net, $mask) = explode('/', $net);
+
+        if (!$mask) {
+            switch (count(explode('.', $net))) {
+                case 1:
+                    $mask = 8;
+                    break;
+                case 2:
+                    $mask = 16;
+                    break;
+                case 3:
+                    $mask = 24;
+                    break;
+                case 4:
+                    $mask = 32;
+            }
+        }
+
+        if (is_string($mask) && strpos($mask, '.') !== false) {
+            $this->maskLong = \ip2long($mask);
+
+            if (!$this->maskLong) {
+                throw new \InvalidArgumentException("Invalid mask: $mask");
+            }
+
+            for ($i = 32; $i > 0; $i--) {
+                if ($this->maskLong === 0xffffffff << (32 - $i)) {
+                    $this->bitmask = $i;
+                    break;
+                }
+            }
+        } elseif ($mask) {
+            $this->bitmask = intval($mask, 10);
+            $this->maskLong = (0xffffffff << (32 - $this->bitmask));
+        } else {
+            throw new \InvalidArgumentException("Invalid mask: empty");
+        }
+
+        $this->netLong = ip2long($net) & $this->maskLong;
+
+        if (!$this->netLong) {
+            throw new \InvalidArgumentException("Invalid net address: $net");
+        }
+
+        if ($this->bitmask > 32) {
+            throw new \InvalidArgumentException("Invalid mask for ip4: $mask");
+        }
+
+        $this->size     = pow(2, 32 - $this->bitmask);
+        $this->base     = long2ip($this->netLong);
+        $this->mask     = long2ip($this->maskLong);
+        $this->hostmask = long2ip(~$this->maskLong);
+
+        $this->first = $this->bitmask <= 30
+                       ? long2ip($this->netLong + 1)
+                       : $this->base;
+
+        $this->last = $this->bitmask <= 30
+                      ? long2ip($this->netLong + $this->size - 2)
+                      : long2ip($this->netLong + $this->size - 1);
+
+        if ($this->bitmask <= 30) {
+            $this->broadcast = long2ip($this->netLong + $this->size - 1);
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->base . '/' . $this->bitmask;
+    }
+
+    public function contains($ip)
+    {
+        if (is_string($ip) &&
+           (strpos($ip, '/') !== false || count(explode('.', $ip)) !== 4)) {
+            $ip = new Netmask($ip);
+        }
+
+        if ($ip instanceof Netmask) {
+            return $this->contains($ip->base)
+                && $this->contains(($ip->broadcast ?: $ip->last));
+        } else {
+            return (ip2long($ip) & $this->maskLong) === ($this->netLong & $this->maskLong);
+        }
+    }
+
+    public function getAll()
+    {
+        $res = array();
+        foreach (range(ip2long($this->first), ip2long($this->last)) as $long) {
+            $res[] = long2ip($long);
+        }
+        return $res;
     }
 }
--- tests/Netmask/NetmaskTest.php
+++ tests/Netmask/NetmaskTest.php
@@ -3,4 +3,80 @@ namespace Netmask;
 
 class NetmaskTest extends \PHPUnit_Framework_TestCase
 {
+    public function provider()
+    {
+        return array(
+            array('192.168.0.1/24', '255.255.255.0'),
+            array('192.168.0.1/255.255.255.0', '255.255.255.0'),
+            array('192.168.0.1', '255.255.255.255')
+        );
+    }
+
+    public function invalidProvider()
+    {
+        return array(
+            array('192.168.0.1/1.1'),
+            array('192.168.0.1.1'),
+            array('192.168.0.1.1/24'),
+            array('192.168.0.1/64')
+        );
+    }
+
+    /**
+     * @covers Netmask\Netmask::__construct
+     * @dataProvider provider
+     */
+    public function testMask($net, $mask)
+    {
+        $block = new Netmask($net);
+        $this->assertEquals($block->mask, $mask);
+    }
+
+    /**
+     * @covers Netmask\Netmask::__construct
+     */
+    public function testProperties()
+    {
+        $block = new Netmask('10.0.0.0/12');
+        $this->assertEquals($block->base, '10.0.0.0');
+        $this->assertEquals($block->mask, '255.240.0.0');
+        $this->assertEquals($block->bitmask, 12);
+        $this->assertEquals($block->hostmask, '0.15.255.255');
+        $this->assertEquals($block->broadcast, '10.15.255.255');
+        $this->assertEquals($block->size, 1048576);
+        $this->assertEquals($block->first, '10.0.0.1');
+        $this->assertEquals($block->last, '10.15.255.254');
+    }
+
+    /**
+     * @covers Netmask\Netmask::contains
+     */
+    public function testContains()
+    {
+        $block = new Netmask('10.0.0.0/12');
+        $this->assertTrue($block->contains('10.8.0.10'));
+        $this->assertTrue($block->contains('10.0.8.10/12'));
+        $this->assertFalse($block->contains('192.168.1.20'));
+    }
+
+    /**
+     * @covers Netmask\Netmask::getAll
+     */
+    public function testGetAll()
+    {
+        $block = new Netmask('192.168.0.1/24');
+        $this->assertTrue(is_array($block->getAll()));
+        $this->assertEquals(254, count($block->getAll()));
+        $this->assertTrue(in_array('192.168.0.24', $block->getAll()));
+    }
+
+    /**
+     * @covers Netmask\Netmask::__construct
+     * @expectedException InvalidArgumentException
+     * @dataProvider invalidProvider
+     */
+    public function testThrows($net)
+    {
+        new Netmask($net);
+    }
 }
